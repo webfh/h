a748039978023d72dce121ffdfd6f4ffa4e21828
甜甜 <145662294+iosyyds@users.noreply.github.com>
2024-08-10 14:38:03 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,5 +1,5 @@
 function build_url(){
     var url = encodeURI(document.querySelector('#url').value);
-    url = "https://erickeven.github.io/wxqq_red/api/?url=" + url;
+    url = "https://webfh.github.io/h/api/?url=" + url;
     document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
 }
